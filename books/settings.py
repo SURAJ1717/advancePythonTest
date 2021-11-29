d839eaa600b70ab9487c441f76b93279dd285112
Suraj Gawhare <surajgawhare@gmail.com>
2021-11-29 14:20:24 +0700

    mysql

--- books/settings.py
+++ books/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'books.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'pythonchallange',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '3306',
-    #     'USER': 'root',
-    #     'PASSWORD': ''
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'defaultdb',
+        'HOST': 'db-mysql-blr1-84705-do-user-3751173-0.b.db.ondigitalocean.com',
+        'PORT': '25060',
+        'USER': 'doadmin',
+        'PASSWORD': 'Yva8eg1vhVeNmavx'
+    }
 }
 
 

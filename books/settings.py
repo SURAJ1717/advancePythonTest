87af37bf513ed16d213079cefce64f8c8600b1c8
Suraj Gawhare <surajgawhare@gmail.com>
2021-11-30 10:26:57 +0700

    firsommit df

--- books/settings.py
+++ books/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c^wf!tw2e+_pak%i1$%(4h$oy1bcp%&on-@wx1apy0i0auq=+&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['3.140.170.188', 'djangofirstapp1717.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
  
 
 # Application definition
